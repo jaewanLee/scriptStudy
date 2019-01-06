9e6c7cf3d0a8260366dd712ccbd418d00b3e7278
jaewanLee <ianlee9180@gmail.com>
2019-01-06 19:22:17 +0900

    [feature]add DBModel on indexSequelize

--- nodejs/sns/models/index.js
+++ nodejs/sns/models/index.js
@@ -1,37 +1,45 @@
-'use strict';
+const Sequelize=require("sequelize")
+const env=process.env.NODE_ENV||"development"
+const config=require("../config/config.json")
+const db={};
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const sequelize=new Sequelize(
+  config.database,confiug.user,config.password,config)
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  db.sequelize=sequelize;
+  db.Sequelize=Sequelize;
+  db.User=require("./user")(sequelize,Sequelize)
+  db.Post=require("./post")(sequelize,Sequelize)
+  db.Hashtag=require("./hashtag")(sequelize,Sequelize)
+
+  //User db는 postDB의 값 여러개를 참조할 수 있다.
+  db.User.hasMany(db.Post)
+  //Post DB는 User DB로부터 1:N관계에 있다.
+  //sequelize에서 자동으로 postDB에 UserDB에 있는 userID컬럼을 추가해준다.
+  db.Post.belongsTo(db.User)
+  //Post DB는 PostHashTag와 다중 참조값을 가질 수 있다.
+  //다중참조관계에 있는 테이블을 자동으로 만들어주는데 그 참조관계가 담겨있는 db이름이 postHasTag이다.
+  //칼럼은 postId와 hashTagId이다.
+  //이렇게함으로써 post에서는 getHashtag/addHastag등이 사용가능하다.
+  //반대로 hastag에서는 getpost/addposts가 가능하다.
+  db.Post.belongsToMany(db.Hashtag,{through:"PostHashTag"})
+  db.Hashtag.belongsToMany(db.Post,{through:"PostHashTag"})
+  //같은 table내에서의 N:M관계.
+  //자기자신이기떄문에 참조키명을 지정해주고, 해당값에는 User의 key값인 userID가 들어간다.
+  //as는 join작업시 사용하는 이름으로, getFolower/addFollower가 가능하다.
+  //Follow라는 테이블을 생성해서 관계를 저장한다.
+  db.User.belongsToMany(db.User,{
+    foreinKey:"follwingId",
+    as:"Followers",
+    through:"Follow"
   })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
+  //자기자신의 참조값을 나타낸것으로 위와 같지만, followerID라는 컬럼에 들어가는 값들이다.
+  //마찬가지로 following를 사용하여 join연산이 가능하다.
+  db.User.belongsToMany(db.User,{
+    foreinKey:"followerId",
+    as:"Followings",
+    through:"Follow"
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+  module.exports=db;
